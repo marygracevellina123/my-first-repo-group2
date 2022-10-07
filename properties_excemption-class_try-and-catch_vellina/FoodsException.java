dd139cc6260736f3be8f1f0350edd486c6bbddbd
PN-LT03\Student.admin <marygracevellina@gmail.com>
2022-10-07 09:16:04 +0800

    My activity thjat is continuation from monday

---
+++ properties_excemption-class_try-and-catch_vellina/FoodsException.java
@@ -0,0 +1,11 @@
+public class FoodsException extends Exception{
+    public FoodsException(String message){
+        super(message);
+    }
+    public String outofBound(){
+        return "Not in the LOOP";
+    }
+    public String negativeIndex(){
+        return "The Index is not Negative";
+    }
+}
---
+++ properties_excemption-class_try-and-catch_vellina/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        String myfavoritefoods[] = {"Burger", "Fried Chiken", "Halo2x", "Mang Inasal", "Humba"};
+        Scanner Scan = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("   Hello, I am Grasya! ");
+        System.out.println("Wanna Know my Favorites? ");
+        System.out.println(" ");
+        System.out.println("  Index of my Fvorite foods!");
+        System.out.println("       ============= ");
+        System.out.println("       =           = ");
+        System.out.println("       = 0 1 2 3 4 = ");
+        System.out.println("       =           = ");
+        System.out.println("       ============= ");
+        System.out.println(" ");
+        System.out.print("Enter the index of my favorite foods you want to know: ");
+        int choice = Scan.nextInt();
+        try{
+            if(choice < 0){
+                Scan.close();
+                System.out.println(" ");
+                throw new FoodsException("The Index is negative, there is no such index in the menu!");
+            }if(choice > 4){
+                Scan.close();
+                System.out.println(" ");
+                throw new FoodsException("There is no index "+ choice +" and no food!");
+            }
+            System.out.println(" ");
+            System.out.println("Yey! " + myfavoritefoods[choice] +" is one of my favorites. I love it" + ".");
+        }catch(FoodsException e){
+            System.out.println(e.getMessage());
+        }
+        Scan.close();
+    }
+}
